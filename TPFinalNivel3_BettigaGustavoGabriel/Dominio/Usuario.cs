003b1a1a99f3cb860ec8b277bbfe0ab0852fa3ab
GabrielBettiga <bettgas468@gmail.com>
2025-11-07 16:42:50 -0300

    Clases del Dominio creadas - Etapa 3 completada
    ✅ Clases creadas dentro del proyecto "Dominio":
    - Marca
    - Categoria
    - Articulo
    - Usuario
    - Favorito

---
+++ TPFinalNivel3_BettigaGustavoGabriel/Dominio/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        string nombre { get; set; }
+        string apellido { get; set; }
+        string email { get; set; }
+        string password { get; set; }
+        string urlImagenPerfil { get; set; }
+        bool esAdmin { get; set; }
+
+
+    }
+}
