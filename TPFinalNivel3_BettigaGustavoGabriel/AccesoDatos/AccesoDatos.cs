222acb175742292554b39f16f4d9c346d43fbc45
GabrielBettiga <bettgas468@gmail.com>
2025-11-08 11:18:33 -0300

    Clase AccesoDatos implementada con conexión, lectura y cierre seguro
    Implementación de la clase AccesoDatos
    
    Funcionalidades actuales:
    - Conexión a la base de datos CATALOGO_WEB_DB mediante SqlConnection.
    - Métodos para configurar consultas SQL directas.
    - Soporte para agregar parámetros dinámicos.
    - Ejecución de lecturas (SELECT) mediante ExecuteReader.
    - Cierre seguro de recursos (lector y conexión) con control de estado.
    
    Estado actual:
    - La clase está totalmente funcional para operaciones de lectura y consulta.

--- TPFinalNivel3_BettigaGustavoGabriel/AccesoDatos/AccesoDatos.cs
+++ TPFinalNivel3_BettigaGustavoGabriel/AccesoDatos/AccesoDatos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,67 @@ namespace AccesoDatos
 {
     public class AccesoDatos
     {
+        private SqlDataReader lector;
+        public SqlConnection conexion { get; }
+        private SqlCommand comando;
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("data source=.\\SQLEXPRESS; initial catalog=CATALOGO_WEB_DB; integrated security=sspi");
+            comando = new SqlCommand();
+            comando.Connection = conexion;
+        }
+
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+
+
+        }
+
+        public void agregarParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+        
+
+        public void ejecutarLectura()
+        {
+            
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            try
+            {
+                // Cerrar el lector si está abierto
+                if (lector != null && !lector.IsClosed)
+                    lector.Close();
+
+                // Cerrar la conexión si existe y está abierta
+                if (conexion != null && conexion.State == System.Data.ConnectionState.Open)
+                    conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
 
 
 
