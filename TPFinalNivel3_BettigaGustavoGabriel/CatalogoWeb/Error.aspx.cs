09fd1341e6d5c2b38561267567da3a9a49c2ae08
GabrielBettiga <bettgas468@gmail.com>
2025-12-11 08:33:21 -0300

    Implementación de pantalla de Error
    Implementación de pantalla de Error

---
+++ TPFinalNivel3_BettigaGustavoGabriel/CatalogoWeb/Error.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CatalogoWeb
+{
+    public partial class Error : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                CargarMensajesDeError();
+        }
+
+        private void CargarMensajesDeError()
+        {
+            // Mensaje amigable para el usuario
+            if (Session["ErrorUsuario"] != null)
+            {
+                lblMensajeError.Text = Session["ErrorUsuario"].ToString();
+            }
+            else
+            {
+                lblMensajeError.Text = "Ocurrió un error inesperado.";
+            }
+
+            // Mensaje técnico para soporte
+            if (Session["ErrorTecnico"] != null)
+            {
+                lblDetalleError.Text = Session["ErrorTecnico"].ToString();
+            }
+            else
+            {
+                lblDetalleError.Text = "No se ha recibido información técnica del error.";
+            }
+
+            // Limpieza de sesión
+            Session.Remove("ErrorUsuario");
+            Session.Remove("ErrorTecnico");
+        }
+    }
+}
---
+++ TPFinalNivel3_BettigaGustavoGabriel/CatalogoWeb/Error.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace CatalogoWeb
+{
+
+
+    public partial class Error
+    {
+
+        /// <summary>
+        /// Control lblMensajeError.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMensajeError;
+
+        /// <summary>
+        /// Control pnlDetalleTecnico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlDetalleTecnico;
+
+        /// <summary>
+        /// Control lblDetalleError.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDetalleError;
+
+        /// <summary>
+        /// Control btnVolverInicio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnVolverInicio;
+    }
+}
